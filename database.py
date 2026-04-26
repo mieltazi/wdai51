081317ee17ff882b2e00bc9b6f79957ae4ec3357
mieltazi <test@test.com>
2026-04-26 22:58:12 +0300

    fix db and add vk widget

--- database.py
+++ database.py
@@ -2,11 +2,8 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-# Код сам возьмет ссылку из настроек Vercel
+# Теперь код будет брать ссылку из безопасного хранилища Vercel
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise ValueError("ОШИБКА: Переменная DATABASE_URL не задана!")
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)

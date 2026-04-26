21b9661384e0dfb7e009eae15d3346e8b25d8e14
mieltazi <test@test.com>
2026-04-26 23:36:39 +0300

    fix supabase connection pooler

--- database.py
+++ database.py
@@ -5,7 +5,13 @@ from sqlalchemy.orm import declarative_base
 # Код берет ссылку из скрытых настроек Vercel
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=False)
+# ВАЖНО: Добавляем connect_args для совместимости с пулером Supabase/PGBouncer
+engine = create_async_engine(
+    DATABASE_URL, 
+    echo=False, 
+    connect_args={"statement_cache_size": 0}
+)
+
 AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 Base = declarative_base()

cf4a92c9c303e596df1a8fe87a220f278fd35bbe
mieltazi <test@test.com>
2026-04-26 23:10:54 +0300

    update

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-# Теперь код будет брать ссылку из безопасного хранилища Vercel
+# Код берет ссылку из скрытых настроек Vercel
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=False)
